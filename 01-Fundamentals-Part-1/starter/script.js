09052a1caf4445dbff13697defc5e861336de997
Manali Joshi <manalijoshi82@gmail.com>
2022-11-16 19:40:09 -0800

    nov-16 updates

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,149 @@
+// let isISland = "USA";
+// let language;
+
+// const country = 'USA';
+// const continent = "North America";
+// let population = 335596890;
+
+// // console.log(typeof isISland);
+// // console.log(typeof population);
+// // console.log(typeof country);
+// // console.log(typeof language);
+
+// language = "English";
+
+// // country = "Brazil";
+// // console.log(language);
+// // console.log(country);
+
+// console.log(population / 2);
+// console.log(population += 1);
+
+// let finlandPopluation = 6000000;
+
+// console.log(finlandPopluation > population);
+// let avgCountryPopulation = 33000000000;
+
+// console.log(avgCountryPopulation < population);
+
+// let description = country + " is in " + continent + " and it's " + population + " people speak " + language;
+
+
+// let description = `${country} is in ${continent} and it's ${population} people speak ${language}`;
+
+// console.log(description);
+
+// let country = 'USA';
+// let continent = "North America";
+// let population = 335596890;
+
+// console.log("I live in " + country + " which is in " + continent + " and has population of" + population);
+
+
+// let numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+// if (numNeighbours === 1) {
+//     console.log(`Only 1 border!`);
+// } else if (numNeighbours > 1) {
+//     console.log(`More than 1 border`);
+// } else if (numNeighbours == 0) {
+//     console.log(`No borders`);
+// }
+
+
+
+
+
+//----------------------------------------------//
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Manali');
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+let year;
+// console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let isISland = "USA";
+let language;
+
+let country = 'USA';
+let continent = "North America";
+let population = 335596890;
+
+console.log(typeof isISland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+let age = 30;
+age = 31;
+
+const birthyear = 1990;
+birthyear = 1992; // doesnt work cause const variable cant be changed
+
+
+const now = 2037
+const  ageManali = now - 1982;
+const ageAmit = now - 1979;
+console.log(ageManali, ageAmit);
+
+console.log(ageManali * 2, ageManali / 11, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+*/
+/*
+const markMass = 78;
+const johnMass = 92;
+
+const markHeight = 1.69;
+const johnHeight = 1.95;
+
+let markBMI = markMass / (markHeight * markHeight);
+let johnBMI = johnMass / (johnHeight * johnHeight);
+
+let markHigherBMI = (markBMI > johnBMI);
+
+console.log(markBMI, johnBMI);
+console.log(markHigherBMI);
+
+markMass = 95;
+johnMass = 85;
+
+markHeight = 1.88;
+johnHeight = 1.76;
+
+markBMI = markMass / (markHeight * markHeight);
+johnBMI = johnMass / (johnHeight * johnHeight);
+
+markHigherBMI = (markBMI > johnBMI)
+
+console.log(markBMI, johnBMI);
+console.log(markHigherBMI);
+
+if (markBMI > johnBMI) {
+    console.log("Mark's BMI is higher than John's");
+}
+console.log("John's BMI is higher than Mark's");
+
+console.log("Mark's BMI " + markBMI + " is higher than " + johnBMI);
+*/
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
